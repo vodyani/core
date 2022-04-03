177ac10d0acd0eff0a9878039b9a30d080c40b3d
John Denver <chogathkk@gmail.com>
2022-04-03 14:58:56 +0800

    style: code review 🌈

--- src/common/type/client.ts
+++ src/common/type/client.ts
@@ -1,4 +1,4 @@
-import { ClientAdapter, AsyncClientAdapter } from '../interface';
+import { ClientAdapter, AsyncClientAdapter } from '../abstract';
 
 export type CreateClientAdapter<T, O> = (options: O, ...args: any[]) => ClientAdapter<T>;
 export type CreateAsyncClientAdapter<T, O> = (options: O, ...args: any[]) => Promise<AsyncClientAdapter<T>>;
