8fcafc1e0153a9564913f82382e4e5c4b2131e6b
John Denver <chogathkk@gmail.com>
2022-04-07 10:01:20 +0800

    style: code

--- src/common/interface/client.ts
+++ src/common/interface/client.ts
@@ -1,3 +1,34 @@
+export interface AnyAdapter {
+  [key: string]: any;
+}
+
+export interface ClientAdapter<T = any> extends AnyAdapter {
+  instance: T;
+  close: (...arg: any) => any;
+}
+
+export interface AsyncClientAdapter<T = any> extends AnyAdapter {
+  instance: T;
+  close: (...arg: any) => Promise<any>;
+}
+
+export interface ClientAdapterProvider<T = any, O = any> {
+  create: (options: O) => ClientAdapter<T>;
+  connect: (key: string) => T;
+}
+
+export interface AsyncClientAdapterProvider<O = any, T = any> {
+  create: (options: O) => Promise<AsyncClientAdapter<T>>;
+  connect: (key: string) => T;
+}
+
+export interface RemoteConfigClient {
+  init: (...args: any[]) => Promise<any>;
+  sync?: (...args: any[]) => Promise<any>;
+  subscribe?: (callback: (config: Record<string, any>) => any) => Promise<any>;
+  close?: (...args: any[]) => Promise<any>;
+}
+
 export interface RemoteConfigDetails<OPTIONS = any, EXTRA = any> {
   key: string;
   options?: OPTIONS;
--- src/common/type/client.ts
+++ src/common/type/client.ts
@@ -1,4 +1,4 @@
-import { ClientAdapter, AsyncClientAdapter } from '../abstract';
+import { ClientAdapter, AsyncClientAdapter } from '../interface';
 
 export type CreateClientAdapter<T = any, O = any> = (options: O, ...args: any[]) => ClientAdapter<T>;
 export type CreateAsyncClientAdapter<T = any, O = any> = (options: O, ...args: any[]) => Promise<AsyncClientAdapter<T>>;
