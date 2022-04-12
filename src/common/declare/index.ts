7ace8c2c0e10f37983722d053eba327f3debfcca
John Denver <chogathkk@gmail.com>
2022-04-12 15:27:53 +0800

    feat: merge deps

--- src/common/declare/index.ts
+++ src/common/declare/index.ts
@@ -1,3 +1,5 @@
 export * from './autobind-decorator';
+export * from './class-transformer';
+export * from './class-validator';
 export * from './fast-safe-stringify';
 export * from './object-hash';
