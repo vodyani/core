7d130b2047e757971aef7305026ae48265624271
John Denver <chogathkk@gmail.com>
2022-04-12 22:52:05 +0800

    fix: api module

--- src/decorator/core/api.ts
+++ src/decorator/core/api.ts
@@ -5,6 +5,9 @@ import { ApiModuleOptions } from '../../common';
 export function ApiRegister(options: ApiModuleOptions) {
   return Module({
     imports: options.imports,
+    exports: [
+      ...(options.consumer || []),
+    ],
     providers: [
       ...(options.aop || []),
       ...(options.consumer || []),
