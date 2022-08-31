f7b075a0b3ebd684b21a0b88359c71b9d4f229c7
John Denver <chogathkk@gmail.com>
2022-08-31 14:16:39 +0800

    fix: add express dependencies

--- src/common/declare.ts
+++ src/common/declare.ts
@@ -1,6 +1,8 @@
 import { HttpAdapterHost } from '@nestjs/core';
 
 export { HttpAdapterHost };
+export { Request, Response, Express } from 'express';
+
 export * from '@nestjs/core';
 export * from '@nestjs/common';
 export * from '@nestjs/platform-express';
