20428be184942464205f702b336cd2b98694275c
John Denver <chogathkk@gmail.com>
2022-09-08 11:04:51 +0800

    fix: subscribe argument

--- src/common/interface/remote-config.ts
+++ src/common/interface/remote-config.ts
@@ -2,7 +2,7 @@ export interface RemoteConfigClient {
   close?: (...args: any[]) => Promise<any>;
   init: (...args: any[]) => Promise<Partial<any>>;
   sync: (...args: any[]) => Promise<Partial<any>>;
-  subscribe?: (key: string, callback: (value: any) => any) => Promise<void>;
+  subscribe?: (key: string, callback: (value: any) => any, ...args: any[]) => Promise<void>;
 }
 
 export interface RemoteConfigInfo<T = any> {
